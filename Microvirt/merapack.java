acd2e2f88553b9ec827a6fb21b60188913894898
Cheersbhaishab <167834675+Cheersbhaishab@users.noreply.github.com>
2024-08-01 09:04:22 +0530

    Add files via upload

---
+++ Microvirt/merapack.java
@@ -0,0 +1,8 @@
+package merapack;//merapack folder name hai 
+public class Mypack//Mypack class name hai 
+{
+public static void P1()//P1() ko method (function) kehte hai 
+{
+System.out.print("hello mera package ban gaya aur run bhi ho rha hai ");
+}
+}
